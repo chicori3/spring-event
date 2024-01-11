e9e46efe1b637944eb8ca64925eaaaa910d4b5b9
sian <chicori3@naver.com>
2024-01-11 21:59:34 +0900

    Initial commit

---
+++ src/main/kotlin/toy/sian/spring/event/SpringEventApplication.kt
@@ -0,0 +1,11 @@
+package toy.sian.spring.event
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringEventApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringEventApplication>(*args)
+}
---
+++ src/test/kotlin/toy/sian/spring/event/SpringEventApplicationTests.kt
@@ -0,0 +1,13 @@
+package toy.sian.spring.event
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringEventApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
