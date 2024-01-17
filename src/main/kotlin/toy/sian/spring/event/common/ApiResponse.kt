f8b6b8ab4a0d81407d62120a50fa1912ff831347
sian <chicori3@naver.com>
2024-01-17 22:21:38 +0900

    feat: 공통 응답 객체 정의

---
+++ src/main/kotlin/toy/sian/spring/event/common/ApiResponse.kt
@@ -0,0 +1,39 @@
+package toy.sian.spring.event.common
+
+data class ApiResponse<T>(
+    val success: Boolean = true,
+    val data: T? = null,
+    val error: Error?,
+) {
+    companion object {
+        fun <T> success(data: T?): ApiResponse<T> {
+            return ApiResponse(
+                success = true,
+                data = data,
+                error = null,
+            )
+        }
+
+        fun <T> fail(
+            code: String,
+            message: String?,
+            properties: Map<String, Any?> = mapOf(),
+        ): ApiResponse<T> {
+            return ApiResponse(
+                success = false,
+                data = null,
+                error = Error(
+                    code = code,
+                    message = message,
+                    properties = properties,
+                )
+            )
+        }
+    }
+}
+
+data class Error(
+    val code: String,
+    val message: String?,
+    val properties: Map<String, Any?> = mapOf(),
+)
