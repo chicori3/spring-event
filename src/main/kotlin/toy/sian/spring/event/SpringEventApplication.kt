3f965aa3ae8f37531927df222437a16d6e35b1cc
sian <chicori3@naver.com>
2024-03-03 18:52:36 +0900

    feat: 비동기 설정 추가

--- src/main/kotlin/toy/sian/spring/event/SpringEventApplication.kt
+++ src/main/kotlin/toy/sian/spring/event/SpringEventApplication.kt
@@ -2,7 +2,9 @@ package toy.sian.spring.event
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableAsync
 
+@EnableAsync
 @SpringBootApplication
 class SpringEventApplication
 
