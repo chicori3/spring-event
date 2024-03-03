7637a7b6f67cad378c2d20a8e2ab2147891263aa
sian <chicori3@naver.com>
2024-03-03 22:22:41 +0900

    feat: jpa auditing 추가

--- src/main/kotlin/toy/sian/spring/event/SpringEventApplication.kt
+++ src/main/kotlin/toy/sian/spring/event/SpringEventApplication.kt
@@ -2,8 +2,10 @@ package toy.sian.spring.event
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.scheduling.annotation.EnableAsync
 
+@EnableJpaAuditing
 @EnableAsync
 @SpringBootApplication
 class SpringEventApplication
