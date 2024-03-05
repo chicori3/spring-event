e2d62d0f9b486e3b06beac996f457f82869b6ad2
sian <chicori3@naver.com>
2024-03-06 01:39:18 +0900

    chore: kotest 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,11 +30,22 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
     implementation("io.github.microutils:kotlin-logging:2.0.11")
+
     runtimeOnly("com.h2database:h2")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    implementation("io.kotest:kotest-property-jvm:5.3.0")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.3.0")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:5.3.0")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+
     testImplementation("io.mockk:mockk:1.13.9")
 }
 
