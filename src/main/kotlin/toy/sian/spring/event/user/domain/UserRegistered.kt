f87a44fa964e3e790e6abde2e98fb527040b6f06
sian <chicori3@naver.com>
2024-03-06 01:41:36 +0900

    refactor: 유저 가입 이벤트 수정

--- src/main/kotlin/toy/sian/spring/event/user/domain/UserRegistered.kt
+++ src/main/kotlin/toy/sian/spring/event/user/domain/UserRegistered.kt
@@ -1,10 +1,9 @@
 package toy.sian.spring.event.user.domain
 
 import toy.sian.spring.event.common.DomainEvent
-import java.time.Instant
 
 data class UserRegistered(
+    val userId: Long,
     val email: String,
     val nickname: String,
-    val createdAt: Instant,
 ) : DomainEvent()
