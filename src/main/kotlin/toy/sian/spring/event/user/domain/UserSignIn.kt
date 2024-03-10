fd34f04dae7df02759f2a2f7b27e04113d3b1126
sian <chicori3@naver.com>
2024-03-10 11:59:54 +0900

    refactor: 유저 로그인 이벤트 네이밍 수정

--- src/main/kotlin/toy/sian/spring/event/user/domain/UserSignIn.kt
+++ src/main/kotlin/toy/sian/spring/event/user/domain/UserSignIn.kt
@@ -3,8 +3,7 @@ package toy.sian.spring.event.user.domain
 import toy.sian.spring.event.common.DomainEvent
 import java.time.Instant
 
-data class UserLoggedIn(
-    val userId: Long,
+data class UserSignIn(
     val email: String,
     val loginAt: Instant,
 ) : DomainEvent()
